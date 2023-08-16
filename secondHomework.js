d449d955c9a0fdae219548eb11c0b3756df388ca
Palienko Mihai <palienko@advantum.pro>
2023-08-16 21:50:41 +0300

    second

---
+++ secondHomework.js
@@ -0,0 +1,14 @@
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30,
+    isStudent: false,
+};
+
+person.firstName = 'Михаил'
+
+person.weight = null
+
+delete person.weight
+
+console.log(person)
