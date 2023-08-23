c696d3f680d55afe8d210ab2e68c094f675478be
Palienko Mihai <palienko@advantum.pro>
2023-08-23 19:12:36 +0300

    [feat] second []

--- secondHomework.js
+++ secondHomework.js
@@ -1,14 +1,72 @@
-const person = {
-    firstName: "John",
-    lastName: "Doe",
-    age: 30,
-    isStudent: false,
-};
+// const key = prompt('Введите свойство')
+// const person = {
+//     age: 30,
+//     isStudent: false,
+//     firstName: "John",
+//     lastName: "Doe",
+// };
 
-person.firstName = 'Михаил'
 
-person.weight = null
+// console.log(person[key])
 
-delete person.weight
 
-console.log(person)
+
+// const person = {
+//     age: 30,
+//     isStudent: false,
+//     firstName: "John",
+//     lastName: "Doe",
+// };
+
+// for(key in person) {
+//     console.log(person[key])
+// }
+
+
+
+// let a = 0
+
+// let b = a
+
+// ++b;
+
+// console.log('b', b);
+// console.log('a', a);
+
+
+
+// const makePerson = (age, isStudent, firstName, lastName) => {
+//     return {
+//         age,
+//         isStudent,
+//         firstName,
+//         lastName,
+//         sayHi: function() {
+//             console.log(`Привет, меня зовут ${this.firstName}. Мне ${this.age} года`)
+//         },
+//     }
+// }
+
+// makePerson(23, false, 'Михай', 'Палиенко')
+
+
+
+// const user = {
+//     name: 'Mihai',
+//     work: {
+//         title: 'Фронтенд',
+//         isRemote: true,
+//     },
+// }
+
+// console.log(user?.work?.title)
+
+
+const surname = document.querySelector('.input').value
+const tel = document.querySelector('.input2').value
+
+
+const user = {
+    surname,
+    tel,
+}
